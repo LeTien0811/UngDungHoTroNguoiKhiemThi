80f270b68c24ef5a72f4d4b356d773c20b4b1277
LeVietTien <letien2081@gmail.com>
2026-01-08 18:47:18 +0700

    Add file to .gitignore

---
+++ lib/services/log_error_services.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class LogErrorServices {
+  final String message;
+  final String type;
+
+  LogErrorServices(
+      this.message,
+      this.type
+      );
+
+
+}
